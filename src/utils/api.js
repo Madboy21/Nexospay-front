71711a93c9d555fc67a6d94984ade8fe25a488bd
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 19:58:34 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,4 @@
 import axios from "axios";
-
-const api = axios.create({ baseURL: "https://nexospay-backend.vercel.app/", headers: { "Content-Type": "application/json" } });
-
-export const getUserStats = (telegramId) => api.post("/api/users/stats", { telegramId });
-export const updateUserProgress = (telegramId, taskName) => api.post("/api/tasks/complete-task", { telegramId, taskName });
-export const claimReferral = (referrerId) => api.post("/api/users/referral", { referrerId });
-export const withdrawRequest = (telegramId, amount, binanceUID) => api.post("/api/withdraw/request", { telegramId, amount, binanceUID });
-
+const api = axios.create({ baseURL:"https://nexospay-backend.vercel.app/", headers:{"Content-Type":"application/json"} });
+export const getUserStats = (telegramId) => api.post("/api/users/stats",{telegramId});
 export default api;
