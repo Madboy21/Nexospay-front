7b2cf32b5f6c047693d3097bf0b699c17db2dd59
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 10:50:22 +0600

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://nexospay-backend.vercel.app/api',
+  baseURL: 'https://nexospay-backend.vercel.app/',
 });
 
 export default API;
