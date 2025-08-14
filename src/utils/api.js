d7cc9000af8bdcf1fbef88a6f45fa5e922b3711e
Madboy21 <justinjordan2211@gmail.com>
2025-08-14 18:17:07 +0600

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,27 +1,10 @@
 import axios from "axios";
 
-const backendUrl = "https://nexospay-backend.vercel.app/"; // change if deployed
+const api = axios.create({ baseURL: "https://nexospay-backend.vercel.app/", headers: { "Content-Type": "application/json" } });
 
-const api = axios.create({
-  baseURL: backendUrl,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-// USER APIs
-export const getUserStats = (telegramId) =>
-  api.post("/api/users/stats", { telegramId });
-
-export const updateUserProgress = (telegramId, taskName) =>
-  api.post("/api/tasks/complete-task", { telegramId, taskName });
-
-export const claimReferral = (referrerId) =>
-  api.post("/api/users/referral", { referrerId });
-
-// WITHDRAW API
-export const withdrawRequest = (telegramId, amount, binanceUID) =>
-  api.post("/api/withdraw/request", { telegramId, amount, binanceUID });
+export const getUserStats = (telegramId) => api.post("/api/users/stats", { telegramId });
+export const updateUserProgress = (telegramId, taskName) => api.post("/api/tasks/complete-task", { telegramId, taskName });
+export const claimReferral = (referrerId) => api.post("/api/users/referral", { referrerId });
+export const withdrawRequest = (telegramId, amount, binanceUID) => api.post("/api/withdraw/request", { telegramId, amount, binanceUID });
 
 export default api;
-
