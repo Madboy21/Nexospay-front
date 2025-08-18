e58d49463f13c315b22af2863ccbdfb636f6b654
Madboy21 <justinjordan2211@gmail.com>
2025-08-18 10:34:19 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,15 +6,16 @@ const api = axios.create({
 });
 
 // Users
-export const registerUser = (userData) => api.post("/api/users/register", userData);
-export const getUserStats = (telegramId) => api.post("/api/users/stats", { telegramId });
-export const claimReferral = (referrerId) => api.post("/api/users/referral", { referrerId });
+export const registerUser = (userData) => api.post("/users/register", userData);
+export const getUserStats = (telegramId) => api.post("/users/stats", { telegramId });
+export const claimReferral = (referrerId) => api.post("/users/referral", { referrerId });
 
 // Tasks
-export const updateUserProgress = (telegramId) => api.post("/api/tasks/complete-task", { telegramId });
+export const updateUserProgress = (telegramId) => api.post("/tasks/complete-task", { telegramId });
 
 // Withdraw
 export const withdrawRequest = (telegramId, amount, binanceUID) =>
-  api.post("/api/withdraw/request", { telegramId, amount, binanceUID });
+  api.post("/withdraw/request", { telegramId, amount, binanceUID });
 
 export default api;
+
