22c18f1a43f042bb3a6434c921c2c5015537dced
Madboy21 <justinjordan2211@gmail.com>
2025-08-18 10:00:22 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nexospay-backend.vercel.app/", // backend URL
+  baseURL: "https://nexospay-backend.vercel.app/api", // backend URL
   headers: { "Content-Type": "application/json" },
 });
 
